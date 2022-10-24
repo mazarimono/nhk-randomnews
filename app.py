a62b47702517f193113ecadaac3a11db4394c21c
mazarimono <mazarimono@gmail.com>
2022-10-24 10:56:59 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from dash import Dash, html, dcc 
+import requests
+
+
+app = Dash('__name__')
+
+r= requests.get('https://nhk-hackathon.herokuapp.com/random_news')
+
+app.layout = html.Div([
+    html.H3(r.json()['news_title']),
+    html.A(r.json()['news_url'], href=r.json()['news_url'])
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
